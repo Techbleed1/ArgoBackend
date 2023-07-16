a9b3ec8514db9500c1fdf74b9f936429cf6d4e85
rockowtham <kumar.gowtham@gmail.com>
2023-07-16 21:03:49 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
+    .setTitle('Argo API Docs')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
