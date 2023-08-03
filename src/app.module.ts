513b9c03277dc7a13cf39b26eb91ab12b4d4bf09
rockowtham <kumar.gowtham@gmail.com>
2023-08-03 17:26:36 +0530

    reverted mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,14 @@ import { ConfigModule } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { PostsModule } from './posts/posts.module';
+import { Src\modules\posts\controllers\postsController } from './src/modules/posts/controllers/posts/src/modules/posts/controllers/posts.controller';
+import { PostsController } from './modules/posts/controllers/posts.controller';
 
 const logger = new Logger('app');
 
-const db_uri1 = 'mongodb://admin:secret@127.0.0.1:27017/dd_argo?auth=flse';
-// const db_uri1 = 'mongodb+srv://kumargowtham:ZFo5DwppYE6cTaME@cluster0.1ujusku.mongodb.net/?retryWrites=true&w=majority';
+// const db_uri1 = 'mongodb://admin:secret@127.0.0.1:27017/dd_argo?auth=flse';
+const db_uri1 = 'mongodb+srv://kumargowtham:ZFo5DwppYE6cTaME@cluster0.1ujusku.mongodb.net/?retryWrites=true&w=majority';
 
 @Module({
   imports: [
@@ -22,8 +25,9 @@ const db_uri1 = 'mongodb://admin:secret@127.0.0.1:27017/dd_argo?auth=flse';
       http: process.env.NODE_ENV !== 'production',
     }),
     UsersModule,
-    AuthModule],
-  controllers: [AppController],
+    AuthModule,
+    PostsModule],
+  controllers: [AppController, Src\modules\posts\controllers\postsController, PostsController],
   providers: [AppService],
 })
 export class AppModule {}
