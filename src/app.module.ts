22a8c097249abe296b5a29e2c3987f07b633bbbd
rockowtham <kumar.gowtham@gmail.com>
2023-08-07 19:05:06 +0530

    reverted mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,6 @@ import { ConfigModule } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
-import { PostsModule } from './posts/posts.module';
-import { Src\modules\posts\controllers\postsController } from './src/modules/posts/controllers/posts/src/modules/posts/controllers/posts.controller';
-import { PostsController } from './modules/posts/controllers/posts.controller';
 
 const logger = new Logger('app');
 
@@ -26,9 +23,8 @@ const db_uri1 = 'mongodb+srv://kumargowtham:ZFo5DwppYE6cTaME@cluster0.1ujusku.mo
       http: process.env.NODE_ENV !== 'production',
     }),
     UsersModule,
-    AuthModule,
-    PostsModule],
-  controllers: [AppController, Src\modules\posts\controllers\postsController, PostsController],
+    AuthModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
