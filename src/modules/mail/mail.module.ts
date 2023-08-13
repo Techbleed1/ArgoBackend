5b9c5ef6aff69cc089cbbc7ee83bcc37ca47bb5a
v-manishs1 <v-manishs1@xiaomi.com>
2023-08-14 02:49:40 +0530

    style-fix

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { MailerService } from './service/mailer.service'; 
+import { MailerService } from './service/mailer.service';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import * as config from 'config';
 import { join } from 'path';
 
 const PROJECT_ROOT = join(__dirname, '..');
 const emailConfig = config.get('email');
-const host      = emailConfig.host;
-const From      = emailConfig.from;
+const host = emailConfig.host;
+const From = emailConfig.from;
 const emailUser = emailConfig.user;
 const emailPass = emailConfig.pass;
 const port = emailConfig.port;
@@ -18,8 +18,8 @@ const port = emailConfig.port;
     MailerModule.forRoot({
       transport: {
         host: host,
-        port: port, 
-        secure: false, 
+        port: port,
+        secure: false,
         auth: {
           user: emailUser,
           pass: emailPass,
@@ -29,16 +29,16 @@ const port = emailConfig.port;
         from: From,
       },
       template: {
-      //  dir: __dirname + '/templates',
+        //  dir: __dirname + '/templates',
         dir: join(PROJECT_ROOT, '../../src/modules/mail/templates'),
         adapter: new HandlebarsAdapter(),
         options: {
           strict: true,
         },
       },
-    }),],
+    }),
+  ],
   providers: [MailerService],
   exports: [MailerService],
 })
-
 export class MailModule {}
